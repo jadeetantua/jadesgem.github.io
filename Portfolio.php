143758e234360fc41a170a0cc751a510878dbef8
Jade <152716268+jadeetantua@users.noreply.github.com>
2024-03-07 01:45:57 +0800

    Add files via upload

---
+++ Portfolio.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jade's Folio</title>
+    <link rel="stylesheet" href="./CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="./CSS/aos.css">
+    <link rel="stylesheet" href="./CSS/line-awesome.min.css">
+    <link rel="stylesheet" href="./CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="./CSS/style.css">
+</head>
+
+<body data-bs-spy="scroll" data-bs-target=".navbar">
+    <!-- NAVBAR -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+        <div class="container flex-lg-column">
+            <a class="navbar-brand mx-lg-auto" href="#">
+                <span class="h3 fw-bold d-block d-lg-none"> Jade's</span>
+                <img src="./IMAGES/jade.jpg" class="d-none d-lg-block rounded-circle" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto flex-lg-column text-lg-center">
+
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#services">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#contact">Contact</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!--//NAVBAR-->
+
+    <!--CONTENT-->
+    <div id="content">
+
+        <!--HOME-->
+        <section id="home" class="full-height bg-lightdark px-lg-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-10">
+                        <h1 class="display-4 fw-bold"> I'M A <span class="text-brand">IT STUDENT</span> FROM NATIONAL
+                            UNIVERSITY</h1>
+                        <p class="lead mt-2 mb-4">I am Diana Jade Tantua, and this is my Website.</p>
+                        <a href="#services" class="btn btn-brand me-3">Explore my Services</a>
+                        <a href="#" class="link-custom">Call: 0928-224-3240</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--//HOME-->
+
+        <!--SERVICES-->
+        <section id="services" class="full-height px-lg-5">
+            <div class="container">
+                <div class="row pb-4">
+                    <div class="col-lg-8">
+                        <h6 class="text-brand">SERVICES</h6>
+                        <h1> Services that I can Provide</h1>
+                    </div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="service p-4 bg-base rounded-4 shadow-effect">
+                            <div class="icombox rounded-4">
+                                <i class="las la-feather"></i>
+                            </div>
+                            <h4 class="mt-4 mb-2">Branding</h4>
+                            <p>I can perform a basic tailored and conversion-focused</p>
+                            <a href="#" class="link-custom">Read More</a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="service p-4 bg-base rounded-4 shadow-effect">
+                            <div class="icombox rounded-4">
+                                <i class="las la-database"></i>
+                            </div>
+                            <h4 class="mt-4 mb-2">Data Encoder</h4>
+                            <p>I can assist you to basic Data Encoding and Analysis </p>
+                            <a href="#" class="link-custom">Read More</a>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="service p-4 bg-base rounded-4 shadow-effect">
+                            <div class="icombox rounded-4">
+                                <i class="las la-pen-fancy"></i>
+                            </div>
+                            <h4 class="mt-4 mb-2">Designing</h4>
+                            <p>I can give you a basic tips and ideas to designing</p>
+                            <a href="#" class="link-custom">Read More</a>
+                        </div>
+                    </div>
+                </div>
+        </section>
+        <!--//SERVICES-->
+
+        <!--ABOUT-->
+        <section id="about" class="full-height px-lg-5">
+            <div class="container">
+                <div class="row pb-4">
+                    <div class="col-lg-8">
+                        <h6 class="text-brand">ABOUT</h6>
+                        <h1>About Myself</h1>
+                    </div>
+                </div>
+
+                <div class="row gy-5">
+                    <div class="col-lg-6">
+                        
+                        <div class="row gy-4">
+
+                            <div class="col-12">
+                                <div class="bg-base p-4 rounded-4 shadow-effect">
+                                    <h4>2nd Year Student</h4>
+                                    <p class="text-brand mb-2">National University Fairview Branch (2022-recent)</p>
+                                    <p class="mb-0">2nd year I.T Student and pursuing Data Analytics</p>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="bg-base p-4 rounded-4 shadow-effect">
+                                    <h4>Practices</h4>
+                                    <p class="text-brand mb-2">Broading my knowledge in Coding</p>
+                                    <p class="mb-0">Currently majoring coding and being persistent to learn more about Database</p>
+                                </div>
+                            </div>
+
+                            <div class="col-12">
+                                <div class="bg-base p-4 rounded-4 shadow-effect">
+                                    <h4>Principle</h4>
+                                    <p class="text-brand mb-2">Aiming higher</p>
+                                    <p class="mb-0">Believing that I will finish the challenges that are thrown in our way as a Student</p>
+                                </div>
+                            </div>
+                            
+
+                            
+
+                        </div>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        </section>
+
+        <!--//ABOUT-->
+
+        <!--CONTACT-->
+        <section id="contact" class="full-height px-lg-5">
+            <div class="container">
+
+                <div class="row justify-content-center text-center">
+                    <div class="col-lg-8 pb-4">
+                        <h6 class="text-brand">CONTACT</h6>
+                        <h1> Interested to be working with me? Contact me.</h1>
+                    </div>
+
+                    <div class="col-lg-8">
+                        <form action="#" class="row g-lg-3 gy-3">
+                            <div class="form-group col-md-6">
+                                <input type="text" class="form-control" placeholder="Your Name">
+                            </div>
+
+                            <div class="form-group col-md-6">
+                                <input type="email" class="form-control" placeholder="Your Email">
+                            </div>
+
+                            <div class="form-group col-12">
+                                <input type="text" class="form-control" placeholder="Input the service type">
+                            </div>
+
+                            <div class="form-group col-12">
+                                <textarea name="" rows="4" class="form-control" placeholder="Enter your message"></textarea>
+                            </div>
+
+                            <div class="form-group col-12 d-grid">
+                                <button type="submit" class="btn btn-brand">Contact Me</button>
+                            </div>
+
+                        </form>
+                    </div>
+
+                    
+                </div>
+            </div>
+        </section>
+        <!--//CONTACT-->
+
+    </div>
+    <!--//CONTENT-->
+
+
+    <script> src = "./JS/bootstrap.bundle.min.js"</script>
+    <script> src = "./JS/aos.js"</script>
+    <script> src = "./JS/main.js"</script>
+</body>
+
+</html>
